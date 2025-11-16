683497ddcfc0d64855e34d874360a5de4666cf62
cheesegoldfish <32321566+cheesegoldfish@users.noreply.github.com>
2025-11-15 23:39:38 -0600

    - PVP Add AutoGuard for anti-lb. - Re-organize shared PVP settings ui.

---
+++ Magitek/Views/UserControls/Common/PvpUtilities.xaml.cs
@@ -0,0 +1,16 @@
+using System.Windows.Controls;
+
+namespace Magitek.Views.UserControls.Common
+{
+    /// <summary>
+    /// Interaction logic for PvpUtilities.xaml
+    /// </summary>
+    public partial class PvpUtilities : UserControl
+    {
+        public PvpUtilities()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
