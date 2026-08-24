c113e0abe3c59c747bf104690d554307303b2676
cheesegoldfish <32321566+cheesegoldfish@users.noreply.github.com>
2026-08-23 23:48:45 -0500

    DRG - Track the correct Life Surge aura

--- Magitek/Utilities/Auras.cs
+++ Magitek/Utilities/Auras.cs
@@ -334,7 +334,7 @@ namespace Magitek.Utilities
             BowShock = 1838,
             DraconianFire = 1863,
             PowerSurge = 2720,
-            LifeSurge = 3259,
+            LifeSurge = 116,
             ChaoticSpring = 2719,
             ChaosThrust = 118,
             LanceCharge = 1864,
